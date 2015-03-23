e7bee169180aead5792bd5daea345cbe682d3c5d
Juan Torres <juanin.torres@gmail.com>
2015-03-23 16:52:17 -0300

    1era Versión Funcional 1.0

---
+++ src/config/menuelement.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+
+    /**
+     * Envoltorio del elemento
+     */
+    'wrapper' => 'li',
+
+    /**
+     * Envoltorio del icono
+     */
+    'wrapperIcon' => 'span',
+
+    /**
+     * Clase del elemento activo
+     */
+    'classCurrentElement' => 'active',
+
+    /**
+     * Clase base de los iconos
+     */
+    'classIcon' => 'glyphicon',
+
+    /**
+     * Clase extra a aplicar a todos los elementos
+     */
+    'classElement' => '',
+
+];
